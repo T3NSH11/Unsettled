e8b46066b4619f9b427905ee8b6d457f3caca577
T3NSH11 <m.zaid@student.saedubai.com>
2021-11-21 15:09:24 +0400

    bug fixes

---
+++ Assets/Scripts/General/Checkpoints.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour
+{
+    public GameObject checkpointsManager;
+    public GameObject player;
+    public float checkpointrange;
+    bool collected;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        checkpointsManager = GameObject.FindGameObjectWithTag("CheckpointsManager");
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < checkpointrange && collected == false)
+        {
+            checkpointsManager.GetComponent<CheckpointsManager>().Checkpoints.Push(gameObject);
+            collected = true;
+        }
+    }
+}
---
+++ Assets/Scripts/General/CheckpointsManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CheckpointsManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject WinUI;
+    public Stack<GameObject> Checkpoints;
+    public GameObject LastCheckpoint;
+    public GameObject ChckCount;    
+    int Collcheckpoints;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Checkpoints = new Stack<GameObject>();
+        ChckCount = GameObject.FindGameObjectWithTag("ChckCount");
+    }
+
+    void Update()
+    {
+        Collcheckpoints = Checkpoints.Count;
+        if (Checkpoints.Count != 0)  
+        {
+            LastCheckpoint = Checkpoints.Peek();
+        }
+
+        if (Collcheckpoints >= 6)
+        {
+            WinUI.SetActive(true);
+        }
+        ChckCount.GetComponent<TMPro.TextMeshProUGUI>().text = Collcheckpoints.ToString();
+    }
+}
